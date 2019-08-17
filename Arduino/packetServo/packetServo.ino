e2b13aa555311a583bb6b9936b518ce806a95fd8
SandBooKota <kipusangu@gmail.com>
2019-08-18 03:12:48 +0900

    3関節スカラに対応

--- Arduino/packetServo/packetServo.ino
+++ Arduino/packetServo/packetServo.ino
@@ -2,14 +2,17 @@
 
 VarSpeedServo myservo1;
 VarSpeedServo myservo2;
+VarSpeedServo myservo3;
 
 const int val180_1=125;
 const int val180_2=130;
+const int val180_3=130;
 //range=250[deg]
 
 void setup() {
   myservo1.attach(9);
   myservo2.attach(10);
+  myservo3.attach(11);
   Serial.begin(9600);
 }
 
@@ -17,13 +20,13 @@ void setup() {
 void loop() {
   if(Serial.available() >= sizeof('H') + sizeof(int)){
 
-    int val[3];
-    int buf[6];
+    int val[7];
+    int buf[14];
     
     if(Serial.read()=='H'){
       delay(50);
       
-      for (byte i = 0 ; i < 6; i++){
+      for (byte i = 0 ; i < 14; i++){
         buf[i] = Serial.read();
         if(buf[i] >= '0' && buf[i] <= '9'){
           buf[i] = buf[i] - 0x30;
@@ -31,19 +34,19 @@ void loop() {
           buf[i] = buf[i] - 0x40 + 9;
         }
       }
-      buf[0] = buf[0] << 4;
-      buf[2] = buf[2] << 4;
-      buf[4] = buf[4] << 4;
-
-      val[0] = buf[0] | buf[1];
-      val[1] = buf[2] | buf[3];
-      val[2] = buf[4] | buf[5];
+      
+      for(byte i = 0; i < 7; i++;){
+        buf[i*2] = buf[i*2] << 4;
+        val[i] = buf[i*2] | buf[i*2+1];
+      }
     }
     
-    myservo1.write(val[0]*val180_1/180,val[2],false);
-    myservo2.write(val[1]*val180_2/180,val[2],false);
+    myservo1.write(val[0]*val180_1/180,val[5],false);
+    myservo2.write(val[1]*val180_2/180,val[5],false);
+    myservo3.write(val[2]*val180_3/180,val[5],false);
     myservo1.wait();
     myservo2.wait();
+    myservo3.wait();
 
     Serial.write('I');
   }
--- robotControl/axis4SCARA.c
+++ robotControl/axis4SCARA.c
@@ -1,50 +1,3 @@
-/*
- * arduino-serial
- * --------------
- *
- * A simple command-line example program showing how a computer can
- * communicate with an Arduino board. Works on any POSIX system (Mac/Unix/PC)
- *
- *
- * Compile with something like:
- *   gcc -o arduino-serial arduino-serial-lib.c arduino-serial.c
- * or use the included Makefile
- *
- * Mac: make sure you have Xcode installed
- * Windows: try MinGW to get GCC
- *
- *
- * Originally created 5 December 2006
- * 2006-2013, Tod E. Kurt, http://todbot.com/blog/
- *
- *
- * Updated 8 December 2006:
- *  Justin McBride discovered B14400 & B28800 aren't in Linux's termios.h.
- *  I've included his patch, but commented out for now.  One really needs a
- *  real make system when doing cross-platform C and I wanted to avoid that
- *  for this little program. Those baudrates aren't used much anyway. :)
- *
- * Updated 26 December 2007:
- *  Added ability to specify a delay (so you can wait for Arduino Diecimila)
- *  Added ability to send a binary byte number
- *
- * Update 31 August 2008:
- *  Added patch to clean up odd baudrates from Andy at hexapodia.org
- *
- * Update 6 April 2012:
- *  Split into a library and app parts, put on github
- *
- * Update 20 Apr 2013:
- *  Small updates to deal with flushing and read backs
- *  Fixed re-opens
- *  Added --flush option
- *  Added --sendline option
- *  Added --eolchar option
- *  Added --timeout option
- *  Added -q/-quiet option
- *
- */
-
 #include <stdio.h>    // Standard input/output definitions
 #include <stdlib.h>
 #include <math.h>
@@ -61,8 +14,8 @@ void error(char* msg)
     exit(EXIT_FAILURE);
 }
 
-int servoWrite(int fd,int deg1,int deg2,int sp);
-int coordinateWrite(int fd,double x,double y,int sp);
+int servoWrite(int fd,int deg1,int deg2,int deg3,int z1, int z2,int sp,int sum);
+int coordinateWrite(int fd,double x,double y,double z,double deg,int sp);
 
 int pre_deg;
 
@@ -97,7 +50,7 @@ int main(int argc, char *argv[])
     exit(EXIT_SUCCESS);
 } // end main
 
-int servoWrite(int fd,int deg1,int deg2,int sp){
+int servoWrite(int fd,int deg1,int deg2,int deg3,int z1, int z2,int sp,int sum)){
     char buf[256];
     char quiet=0;
     int rc;
@@ -123,31 +76,36 @@ int servoWrite(int fd,int deg1,int deg2,int sp){
 
 }
 
-int coordinateWrite(int fd,double x,double y,int sp){
+int coordinateWrite(int fd,double x,double y,double z,double deg,int sp){
     char buf[256];
     char quiet=0;
     int rc;
 
-    double l1=127,l2=127;
+    double l1=127,l2=127,l3;
     double A,B,C,D;
-    int deg1,deg2;
-    double th1,th2;
+    int deg1,deg2,deg3;
+    double th1,th2,th2;
+	double theta = deg * 180/PI;
+
+	int z1,z2,sum;
 
     //逆運動学
-	A = pow(x,2)+pow(y,2)-pow(l1,2)-pow(l2,2);
-	B = sqrt( 4 - ( pow(A,2)/(pow(l1,2)*pow(l2,2)) ));
-	C = l1 + A/(2*l1);
-	th1 = atan(y/x) - atan((l2*B)/(2*C));
+	A = y - l3*sin(theta);
+	B = x - l3*cos(theta);
+	C = (A*A + B*B + l1*l1 - l2*l2)/(2*l1);
+	th1 = atan2(a,b) - atan2(+sqrt(A*A+B*B-C*C), C);
+	
+	D = (A*A + B*B - l1*l1 + l2*l2)/(2*l2);   
+	th2 = atan2(+sqrt(A*A+B*B-C*C), C) - atan2(-sqrt(A*A+B*B-D*D), D);
 
-	D = A/(2*l1*l2);
-	th2 = acos(D);
+	th3 = theta - th1 - th2;
 
-	//printf("th1:%f th2:%f\n",th1,th2);
 	deg1=215-th1*180/PI;
 	deg2=125-th2*180/PI;
-	printf("%.2f deg1:%d deg2:%d\n",y,deg1,deg2);
+	deg3=215-th3*180/PI;
+	printf("deg1:%d deg2:%d deg3:%d\n",deg1,deg2,deg3);
 
-	snprintf(buf, 12, "H%02X%02X%02X", deg1, deg2, sp);
+	snprintf(buf, 28, "H%02X%02X%02X%02X%02X%02X%02X", deg1, deg2, deg3, z1, z2, sp, sum);
     if( !quiet ) printf("send string:%s\n", buf);
 	rc = serialport_write(fd, buf);
     if(rc==-1) error("error writing");
